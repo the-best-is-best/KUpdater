c43156bbb1f2e2bcddce5744065cfb58ab03eb38
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-04-07 11:33:22 +0200

    Update Swift tools version and add platform support for iOS 12

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KUpdater",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
