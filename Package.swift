ed1396b02774ea6dc66081e71d2dbffc5a017f10
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-10-27 17:59:00 +0300

    Add swiftSettings to KUpdater target for Live Activity support

--- Package.swift
+++ Package.swift
@@ -12,13 +12,22 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KUpdater",
-            targets: ["KUpdater"]),
+            targets: ["KUpdater"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KUpdater"),
+            name: "KUpdater",
+            // swiftSettings: [
+            //               .define("LIVE_ACTIVITY_ENABLED", .when(platforms: [.iOS], configuration: .release)),
+            //                .unsafeFlags([
+            //                    "-emit-objc-header",
+            //                    "-emit-objc-header-path", "./Headers/KUpdater-Swift.h"
+            //                ])
+            //           ]
+
+        )
 
     ]
 )
